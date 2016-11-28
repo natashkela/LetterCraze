6c51744006c89efac08c837e135ad46eee3ec0bf
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-11-29 03:36:13 +0800

    Create Board.java

---
+++ Builder/Board.java
@@ -0,0 +1,30 @@
+package Player;
+
+public class Board {
+	
+	Tile[][] tiles;//2D array for tiles
+		
+		public Board(Tile[][] tiles){
+			if((tiles.length==6) && (tiles[0].length==6)){ //Check if its greater than 36 tiles
+				this.tiles = tiles;
+			}
+			else{
+				System.err.println ("The number of tiles is greater than 36. Please change it.");
+			}
+		}
+		
+		Tile[][] getTile(){//get the tiles
+			return this.tiles;
+		}
+		
+		boolean setTile(Tile[][] tile){//boolean for setting tiles. if set return true, else return false
+			if((tiles.length == 6)&&(tiles[0].length == 6)){
+				this.tiles = tiles;
+				return true;
+			}
+			else{ 
+				return false;
+			}
+		}
+
+}
